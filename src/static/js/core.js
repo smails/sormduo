1b3b34b98ed057aedc1fa4f0f99ae128d6861ed0
smails <maxon2504@gmail.com>
2024-10-29 20:47:43 +0300

    popup

--- src/static/js/core.js
+++ src/static/js/core.js
@@ -5,9 +5,9 @@ $('.js-open-modal').click((e) => {
   $('.modal').hide();
   const modal = e.target.getAttribute('href') != null ? e.target.getAttribute('href') : e.target.parentElement.getAttribute('href');
   $(`[data-modal="${modal}"]`).fadeIn();
-  if(modal == '#place') getPlaceModalSlider();
-  if(modal == '#finish') getFinishModalSlider();
-  if(modal == '#plane') getPlaneModalSlider();
+  if (modal == '#place') getPlaceModalSlider();
+  if (modal == '#finish') getFinishModalSlider();
+  if (modal == '#plane') getPlaneModalSlider();
 });
 
 $('.js-close-modal').click((e) => {
@@ -15,9 +15,9 @@ $('.js-close-modal').click((e) => {
   $('.modal').fadeOut();
 });
 
+getBuildhModalSlider()
 
-
-function getPlaceModalSlider(){
+function getPlaceModalSlider() {
   $('.place-modal__slider').slick({
     arrows: true,
     slidesToShow: 1,
@@ -34,7 +34,7 @@ function getPlaceModalSlider(){
   });
 }
 
-function getFinishModalSlider(){
+function getFinishModalSlider() {
   $('.modal-finish__slider').slick({
     arrows: true,
     slidesToShow: 1,
@@ -50,7 +50,31 @@ function getFinishModalSlider(){
     }]
   });
 }
-$('.modal-finish__btn').click(function(e) {
+
+function getBuildhModalSlider() {
+  $('.modal-work__slider').slick({
+    arrows: true,
+    slidesToShow: 1,
+    appendArrows: $('.modal-work').find('.slider-arrows'),
+    prevArrow: '<button id="prev" type="button" class="slider-arrow slider-arrow_prev"><svg xmlns="http://www.w3.org/2000/svg" width="6" height="8" fill="none"><path fill="#006E90" d="M.87287 3.24407c-.46046.39877-.46046 1.11309 0 1.51186l3.47248 3.00725C4.99299 8.32406 6 7.864 6 7.00725V.99275C6 .136 4.99299-.32405 4.34535.23682L.87287 3.24407Z"/></svg></button>',
+    nextArrow: '<button id="next" type="button" class="slider-arrow slider-arrow_next"><svg xmlns="http://www.w3.org/2000/svg" width="6" height="8" fill="none"><path fill="#006E90" d="M5.12713 3.24407c.46046.39877.46046 1.11309 0 1.51186L1.65465 7.76318C1.00701 8.32406 0 7.864 0 7.00725V.99275C0 .136 1.00701-.32405 1.65465.23682l3.47248 3.00725Z"/></svg></button>',
+    dots: true,
+    appendDots: $('.modal-work').find('.slider-dots'),
+    customPaging: function (slider, i) {
+      console.log(slider)
+      return (i + 1) + '/' + slider.slideCount;
+    },
+    responsive: [{
+      breakpoint: 600,
+      settings: {
+        dots: true,
+        arrows: false
+      }
+    }]
+  });
+}
+
+$('.modal-finish__btn').click(function (e) {
   e.preventDefault();
   $(this).addClass('active').siblings().removeClass('active');
   const slideno = $(this).data('slide');
@@ -58,7 +82,7 @@ $('.modal-finish__btn').click(function(e) {
 });
 
 
-function getPlaneModalSlider(){
+function getPlaneModalSlider() {
   $('.modal-plane__slider').slick({
     arrows: true,
     slidesToShow: 1,
