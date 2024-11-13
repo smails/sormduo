c39647c8567bda920c1fbac259994f30eed99ae4
smails <maxon2504@gmail.com>
2024-11-13 20:23:01 +0300

    slider

--- src/modules/proposal/proposal.js
+++ src/modules/proposal/proposal.js
@@ -2,7 +2,7 @@ import $ from 'jquery'
 import '../../static/js/jquery-ui.js'
 
 
-$(".proposal__group .slider").slider({
+$(".squere .slider").slider({
   animate: "slow",
   range: true,
   min: 1,
@@ -10,11 +10,28 @@ $(".proposal__group .slider").slider({
   values: [1,25],
   step: 0.1,
   slide : function(event, ui) {    
-      $(".proposal__group .range-start").text( ui.values[ 0 ]);       
-      $(".proposal__group .range-end").text( ui.values[ 1 ]);
+      $(".squere .range-start").text( ui.values[ 0 ]);       
+      $(".squere .range-end").text( ui.values[ 1 ]);
   },
   stop  : function(event, ui) {    
-      $(".proposal__group .price_start").val( ui.values[ 0 ] ).change(); 
-      $(".proposal__group .price_end").val( ui.values[ 1 ] ).change(); 
+      $(".squere .price_start").val( ui.values[ 0 ] ).change(); 
+      $(".squere .price_end").val( ui.values[ 1 ] ).change(); 
+  }
+});
+
+$(".price .slider").slider({
+  animate: "slow",
+  range: true,
+  min: 1,
+  max: 25,
+  values: [1,25],
+  step: 0.1,
+  slide : function(event, ui) {    
+      $(".price .range-start").text( ui.values[ 0 ]);       
+      $(".price .range-end").text( ui.values[ 1 ]);
+  },
+  stop  : function(event, ui) {    
+      $(".price .price_start").val( ui.values[ 0 ] ).change(); 
+      $(".price .price_end").val( ui.values[ 1 ] ).change(); 
   }
 });
